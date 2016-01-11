f1c3039a4a8f2ea832b59d85ede321b319db496c
Daniel Lombraña González <teleyinex@gmail.com>
2016-01-11 15:11:55 +0100

    Test create photoset works.

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -38,3 +38,23 @@ class TestHelpers(TestDefault):
         flickr.photosets.getList.return_value = self.photoset_list
         resp = _get_photoset_id(flickr, "Something")
         assert resp == None, resp
+
+    @patch('helpers._get_photoset_id')
+    def test_create_photoset(self, mock):
+        """Test create_photoset method works."""
+        mock.return_value = None
+        flickr = MagicMock()
+        flickr.photosets.create.return_value = dict(stat='ok')
+        resp = _create_photoset(flickr, "Raspberry", 1)
+        flickr.photosets.create.assert_called_with(title='Raspberry',
+                                                   primary_photo_id=1)
+        assert resp['stat'] == 'ok'
+
+    @patch('helpers._get_photoset_id')
+    def test_create_photoset_exists(self, mock):
+        """Test create_photoset exists method works."""
+        mock.return_value = 1
+        flickr = MagicMock()
+        resp = _create_photoset(flickr, "Raspberry", 1)
+        assert resp['stat'] == 'exists', resp
+        assert resp['photoset_id'] == 1, resp
